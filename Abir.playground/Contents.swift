383ac7f90209bd7a58cb68d54811c27052844aba
Abir <98805529+Abir-Bin-Billal@users.noreply.github.com>
2022-02-02 18:11:21 +0600

    Initial commit

---
+++ Abir.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+let A = ""
+if A.isEmpty {
+    print("Yes..its empty")
+    
+}else{
+    print("no..")
+}
+var optionalNumber : Int?
+optionalNumber = 28
+/*if let abir = optionalNumber {
+    print("I have a value and it is a \(abir)")
+    
+}else{
+    print("nope")
+}*/
+
