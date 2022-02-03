c6a9a8b72320b1997076f5d2272b6b3f6d13d681
Abir <98805529+Abir-Bin-Billal@users.noreply.github.com>
2022-02-03 20:55:06 +0600

    Data_types

--- Data types.playground/Contents.swift
+++ Data types.playground/Contents.swift
@@ -1,3 +1,20 @@
-import UIKit
+var a:String = "i have a pen"
+
+var b:Int = 295467
+
+var c:Float = 10.7
+
+var d:Double = 107.4646
+
+var e:Bool = false
+d.round()
+  print(d)
+
+  print(Int(c))
+
+  print(Double(b))
+
+  print(Float(d))
+
 
 
