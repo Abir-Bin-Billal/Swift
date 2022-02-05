cd9708777db81795e5ff526346be6265b440ea44
Abir <98805529+Abir-Bin-Billal@users.noreply.github.com>
2022-02-05 09:23:14 +0600

    Condition
    Condition

---
+++ If Statement.playground/Contents.swift
@@ -0,0 +1,25 @@
+var a = 8
+
+var b = 5
+
+var c = 26
+
+var d = 14
+
+if a<6 {
+    print("yes..its value is only 6")
+}else{
+    print("no..its value is not 6")
+}
+if b > 4 && a == 8{
+    print("b is gretter than 4")
+}else{
+    print("no..its wrong!")
+}
+if b < d  {
+    print("b is gretter thn d")
+}else if  c == 26 || d > 23{
+    print("Nothing is impossible")
+}
+
+
